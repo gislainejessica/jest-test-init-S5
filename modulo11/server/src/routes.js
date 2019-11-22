5a07e6849de64fcdae3b2eaf2b4df16104bdf21c
GislaineJessica <GislainehJessica@gmail.com>
2019-11-22 11:29:55 -0300

    Teste de criaçao de usuario

--- modulo11/server/src/routes.js
+++ modulo11/server/src/routes.js
@@ -1,7 +1,8 @@
 import { Router } from 'express';
+import UseController from './app/controllers/UserController';
 
 const routes = new Router();
 
 routes.get('/', (req, res) => res.json({ message: 'Welcome to Omni CLI' }));
-
+routes.post('/users', UseController.store);
 export default routes;
